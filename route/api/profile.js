90803913440f12e51e41c7b8347046f53e317d82
Samad Khan <khansamad99>
2020-01-12 12:57:37 +0530

    Added Delete Route

--- route/api/profile.js
+++ route/api/profile.js
@@ -105,7 +105,7 @@ router.post(
 
 
 //@route POST api/profile/users/:user_id
-//@desc  Create or update a user profile
+//@desc  
 //@access PUBLIC
 router.get('/users/:user_id',async (req,res) => {
     try {
@@ -122,5 +122,79 @@ router.get('/users/:user_id',async (req,res) => {
         console.log(err.message);
         res.status(500).send('Server Error');
     }
-})
+});
+
+
+
+//@route DELETE api/profile
+//@desc  Delete profile,users and posts
+//@access PRIVATE
+
+router.delete('/',auth, async(req,res) => {
+  try {
+
+    //Remove Profile
+    await Profile.findOneAndRemove({user:req.user.id});
+    //Remove users
+    await User.findOneAndRemove({user:req.user.id});
+
+    res.json({msg:'User Deleted'});
+  } catch (err) {
+      console.error(err.message);
+      res.status(400).send('Server Error');
+  }
+
+});
+
+router.put('/experience' , 
+  [
+    auth,
+      [
+        check('title','Title is Required')
+          .not()
+          .isEmpty(),
+        check('company','Company is Required')
+          .not()
+          .isEmpty(),
+        check('from','From date is required')
+          .not()
+          .isEmpty()    
+          
+      ]
+  ], async(req,res) =>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      res.status(400).json({errors:errors.array()});
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    }
+
+    try {
+      const profile = await Profile.findOne({user:req.user.id});
+
+      
+    } catch (err) {
+      console.error(err.message);
+      res.status(400).send('Server Error');
+    }
+  }
+);
 module.exports = router;
