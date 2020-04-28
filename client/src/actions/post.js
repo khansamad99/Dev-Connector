4fb6b691447c264a38f2871108d86f7c78183d59
Samad Khan <khansamad99>
2020-04-28 16:32:35 +0530

    Added Commnents and Likes Feature

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {setAlert} from './alert'
-import {GET_POSTS,POST_ERROR,UPDATE_LIKES,DELETE_POST} from './types';
+import {GET_POSTS,POST_ERROR,UPDATE_LIKES,DELETE_POST,ADD_POST,ADD_COMMENT,REMOVE_COMMENT} from './types';
 
 export const getPosts = () => async dispatch => {
     try {
@@ -52,13 +52,52 @@ export const addLike = id => async dispatch => {
 //Delete Post
 export const deletePost = id => async dispatch => {
     try {
-        const res = await axios.delete(`/api/posts/${id}`);
+        await axios.delete(`/api/posts/${id}`);
         dispatch({
             type:DELETE_POST,
             payload:id
         });
 
         dispatch(setAlert('Post Removed','success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+          });
+    }
+}
+
+// Add Post
+export const addPost = formData => async dispatch => {
+    const config = {
+        headers : {
+            'Content-Type':'application/json'
+        }
+    }
+    try {
+        const res = await axios.post(`/api/posts/`,formData,config);
+        dispatch({
+            type:ADD_POST,
+            payload:res.data
+        });
+
+        dispatch(setAlert('Post Created','success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+          });
+    }
+}
+
+//Get Single Post
+export const getPost = id => async dispatch => {
+    try {
+        const res = await axios.get(`./api/posts/${id}`);
+        dispatch({
+            type:GET_POST,
+            payload:res.data
+        });
     } catch (err) {
         dispatch({
             type: POST_ERROR,
